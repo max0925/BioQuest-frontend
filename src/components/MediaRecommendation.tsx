f6fb9c4ae737b448c2d364f3c4e34cd3e7b8d8ab
max0925 <maxbian0925@gmail.com>
2025-04-08 22:55:03 -0400

    Update MediaRecommendation.tsx

--- src/components/MediaRecommendation.tsx
+++ src/components/MediaRecommendation.tsx
@@ -74,7 +74,6 @@ export default function MediaRecommendation() {
           src={url}
           alt="Recommended visual"
           className="rounded-xl w-full md:w-1/2 h-60 object-cover"
-           layout="responsive"  // 使用 responsive 布局
           width={500} // 请根据实际大小调整宽高
           height={500}
         />
