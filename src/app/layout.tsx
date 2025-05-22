28869853fa841afe908262208cffece58802f6f8
max0925 <maxbian0925@gmail.com>
2025-05-22 18:46:19 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+    {/* ✅ GoatCounter 统计脚本 */}
+    <script
+      data-goatcounter="https://bioquest.goatcounter.com/count"
+      async
+      src="//gc.zgo.at/count.js"
+    ></script>
+  </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
